04498bf6f73267665c9ad9509c43d7b73b8d0f2e
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-07 16:11:20 +0900

    feat: completed H task

--- train.ts
+++ train.ts
@@ -1,25 +1,20 @@
-/* G-TASK: 
+/* H-TASK: 
 
-Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
-MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([]) return qiladi "12"
 
 @MITASK
 */
 
-function getHighestIndex(getNumber: number[]) {
-  let ans: any = 0;
+function getPositive(getNumber: any) {
+  let ans: number[] = [];
 
   for (let i = 0; i < getNumber.length; i++) {
-    if (getNumber[i] > ans) {
-      ans = getNumber[i];
+    if (getNumber[i] > 0) {
+      ans.push(getNumber[i]);
     }
   }
-  if (ans >= 10) {
-    let showNumber = ans.toString().split("").join("");
-    console.log(parseInt(showNumber[1]));
-  } else {
-    console.log("Please, try again");
-  }
+  return ans.join("");
 }
 
-getHighestIndex([5, 21, 12, 21, 8]);
+console.log(getPositive([1, -4, 2]));
