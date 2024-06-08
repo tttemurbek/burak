bba6251ed26176d09bde969f8f35dad0592670b4
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-08 23:19:33 +0900

    feat: member service model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,6 +1,7 @@
 // controllerlarni har dayim object usilinda paydalanamiz
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import MemberService from "../models/Member.service";
 
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
