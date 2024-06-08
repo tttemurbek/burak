a8ce65f5b148bf86059f8daff90d39c2a8b461a0
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-08 22:43:46 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 /** 1-ENTERANCE **/
 
 const app = express();
@@ -15,6 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/", router); // middleware Design pattern
+app.use("/ADMIN", routerAdmin); // BSSR: EJS
+app.use("/", router); // SPA: REACT
 
 export default app;
---
+++ src/controllers/restaurant.controller.ts
@@ -0,0 +1,30 @@
+// controllerlarni har dayim object usilinda paydalanamiz
+import { Request, Response } from "express";
+import { T } from "../libs/types/common";
+
+const restaurantController: T = {};
+restaurantController.goHome = (req: Request, res: Response) => {
+  try {
+    res.send("Homepage");
+  } catch (err) {
+    console.log("Error goHome", err);
+  }
+};
+
+restaurantController.getLogin = (req: Request, res: Response) => {
+  try {
+    res.send("Login page");
+  } catch (err) {
+    console.log("Error getLogin", err);
+  }
+};
+
+restaurantController.getSignup = (req: Request, res: Response) => {
+  try {
+    res.send("Signup page");
+  } catch (err) {
+    console.log("Error getSignup", err);
+  }
+};
+
+export default restaurantController;
