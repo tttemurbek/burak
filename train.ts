50c784dafe3261124f3906dda3645910e2719ab7
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-16 16:00:40 +0900

    feat: solved I task

--- train.ts
+++ train.ts
@@ -1,3 +1,31 @@
+/* I-TASK:
+
+Shunday function yozing, u parametridagi array ichida eng kop takrorlangan raqamni topib qaytarsin.
+MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4
+
+@MITASK*/
+
+function majorityElement(nums: number[]) {
+  let counter: number = 0;
+  let salt: number = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (counter === 0) {
+      salt = nums[i];
+    } else {
+      if (nums[i] === salt) {
+        counter++;
+      } else {
+        counter--;
+      }
+    }
+  }
+
+  return salt;
+}
+
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
+
 /* PROJECT STANDARDS
 -   logging standards
 -   naming stardards
