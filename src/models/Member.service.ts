1daa1fc0eb265f5f886b35048539847bac47c4ef
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-16 14:42:03 +0900

    fix: modify restauratn login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import { MemberInput, LoginInput, Member } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
 import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -13,8 +14,13 @@ class MemberService {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    console.log("before", input.memberPassword);
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    console.log("after", input.memberPassword);
+
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = "";
@@ -36,7 +42,12 @@ class MemberService {
       throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
     }
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+    // const isMatch = input.memberPassword === member.memberPassword;
+
     if (!isMatch) {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
     }
