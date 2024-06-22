a54e28fd3b73a7598fe9a0e9225e6141a581aa3a
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-23 02:10:42 +0900

    feat: Configuration of Product Schema model

---
+++ src/schema/Product.model.ts
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductCollection,
+  ProductSize,
+  ProductStatus,
+  ProductVolume,
+} from "../libs/enums/product.enum";
+
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.PAUSE,
+    },
+
+    pruductCollection: {
+      type: String,
+      enum: ProductCollection,
+      required: true,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      default: ProductSize.NORMAL,
+    },
+
+    productVolume: {
+      type: String,
+      enum: ProductVolume,
+      default: ProductVolume.ONE,
+    },
+
+    productDesc: {
+      type: String,
+      required: true,
+    },
+
+    productImages: {
+      type: [String],
+      default: [],
+    },
+
+    productView: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } //updateAt, createAt
+);
+
+productSchema.index(
+  { productName: 1, productSize: 1, productVolume: 1 },
+  { unique: true }
+);
+
+export default mongoose.model("Product", productSchema);
+
+/* valiadtions, 4 types
+1: frontend valiadtion
+pipe valiadtion
+2: backend valiadtion
+3: database valiadtion
+*/
+
+/* cookies
+request join
+self destroy
+*/
