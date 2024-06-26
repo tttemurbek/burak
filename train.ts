28145872404c6da78069551d62d6f824099561ed
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-26 19:50:29 +0900

    feat: completed M-TASK

--- train.ts
+++ train.ts
@@ -1,14 +1,24 @@
-function reverseSentence(str: string): string {
-  const words: string[] = str.split(" ");
+/* M-TASK: 
 
-  const reversedWords: string[] = [];
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
 
-  for (let i = 0; i < words.length; i++) {
-    const reversedWord: string = words[i].split("").reverse().join("");
-    reversedWords.push(reversedWord);
+@MITASK
+*/
+
+function getSquareNumbers(
+  input: number[]
+): { number: number; square: number }[] {
+  let result: { number: number; square: number }[] = [];
+
+  for (let i = 0; i < input.length; i++) {
+    result.push({
+      number: input[i],
+      square: input[i] * input[i],
+    });
   }
 
-  return reversedWords.join(" ");
+  return result;
 }
 
-console.log(reverseSentence("i love coding"));
+console.log(getSquareNumbers([1, 88, 3]));
