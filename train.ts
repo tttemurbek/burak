2aa4cbcc18bb1a8f8d43edbc71dc3cc2236cc666
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-28 16:11:03 +0900

    feat: completed N-TASK

--- train.ts
+++ train.ts
@@ -1,24 +1,18 @@
-/* M-TASK: 
+/* N-TASK: 
 
-Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
-MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
 
 @MITASK
 */
 
-function getSquareNumbers(
-  input: number[]
-): { number: number; square: number }[] {
-  let result: { number: number; square: number }[] = [];
-
-  for (let i = 0; i < input.length; i++) {
-    result.push({
-      number: input[i],
-      square: input[i] * input[i],
-    });
+function palindromCheck(str: string): boolean {
+  let reversed = str.split("").reverse().join("");
+  if (reversed === str) {
+    return true;
+  } else {
+    return false;
   }
-
-  return result;
 }
 
-console.log(getSquareNumbers([1, 88, 3]));
+console.log(palindromCheck("eye"));
