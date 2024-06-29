a8c1f7383f73c86265f5a3882ca60b1bb795b5a2
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-30 01:43:20 +0900

    fix: correct login process logic

--- train.ts
+++ train.ts
@@ -1,18 +1,12 @@
-/* N-TASK: 
-
-Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
-MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
-
-@MITASK
+/* validation types
+    frontend
+    backend
+    schema
+    dto
 */
 
-function palindromCheck(str: string): boolean {
-  let reversed = str.split("").reverse().join("");
-  if (reversed === str) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(palindromCheck("eye"));
+/*requst types
+    traditional api (form  POST )
+    rest api
+    graphql api
+ */
