3b638f5ad66b84bcd2fe3828f5f2fcd4449d9464
tttemurbek <nonemenoneme9999@gmail.com>
2024-06-30 14:27:23 +0900

    feat: completed O-TASK

--- train.ts
+++ train.ts
@@ -10,3 +10,20 @@
     rest api
     graphql api
  */
+
+/* O-TASK:
+
+Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
+MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45
+*/
+
+function calculateSumOfNumbers(arr: any[]): number {
+  return arr.reduce((sum, current) => {
+    if (typeof current === "number") {
+      return sum + current;
+    }
+    return sum;
+  }, 0);
+}
+
+console.log(calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]));
