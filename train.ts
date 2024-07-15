ff7ff1b6ec969e2417ad622f7038f106a576fa6e
tttemurbek <nonemenoneme9999@gmail.com>
2024-07-15 21:40:20 +0900

    feat: completed U-TASK

--- train.ts
+++ train.ts
@@ -1,34 +1,18 @@
 /* 
-T-TASK:
+U-TASK:
 
-Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
-MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+Shunday function yozing, uni number parametri bolsin va 0 dan berilgan parametrgacha bolgan oraliqdagi faqat toq sonlar nechtaligini return qilsin
+MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
 */
 
-function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
-  let returnArray: number[] = [];
-  let i = 0,
-    j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      returnArray.push(arr1[i]);
-      i++;
-    } else {
-      returnArray.push(arr2[j]);
-      j++;
-    }
+function sumOdds(num: number): number {
+  if (num >= 0 && num % 2 === 0) {
+    let counter = num / 2;
+    return counter;
+  } else {
+    let coutner = (num - 1) / 2;
+    return coutner;
   }
-
-  for (; i < arr1.length; i++) {
-    returnArray.push(arr1[i]);
-  }
-
-  for (; j < arr2.length; j++) {
-    returnArray.push(arr2[j]);
-  }
-
-  return returnArray;
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(sumOdds(12));
