983d11454f0529288b33805b65353e256dc5c4a6
tttemurbek <nonemenoneme9999@gmail.com>
2024-07-19 01:18:00 +0900

    feat: completed W-TASK

--- train.ts
+++ train.ts
@@ -1,22 +1,26 @@
 /* 
-V-TASK:
+W-TASK:
 
-Shunday function yozing, uni string parametri bolsin va stringdagi harf va u harf necha marta takrorlangani sonidan tashkil topgan object qaytarsin.
-MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
+Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
+MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
+
+@MITASK
 */
 
-function countChars(str: string): { [key: string]: number } {
-  const charCounter: { [key: string]: number } = {};
+function chunkArray(array: any[], size: number): any[][] {
+  const result: any[][] = [];
 
-  for (let char of str) {
-    if (charCounter[char]) {
-      charCounter[char]++;
-    } else {
-      charCounter[char] = 1;
-    }
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    result.push(chunk);
   }
 
-  return charCounter;
+  return result;
 }
 
-console.log(countChars("absdsdsajhwqirohfbjhdabszdniqlkafm"));
+console.log(
+  chunkArray(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    5
+  )
+);
