e27d573c8cd5b1a1f776aeaa3b99fce54491e40f
tttemurbek <nonemenoneme9999@gmail.com>
2024-08-07 18:55:24 +0900

    feat: completed ZF-TASK

--- train.ts
+++ train.ts
@@ -1,24 +1,24 @@
-/** 
- * ZE-TASK:
+/**
+ZF-TASK:
 
-Shunday function yozing, uni  string parametri bolsin. String ichida takrorlangan harflarni olib tashlab qolganini qaytarsin
-MASALAN: removeDuplicate('stringg') return 'string'
+Shunday function yozing, uni string parametri bolsin. String ichidagi har bir sozni bosh harflarini katta harf qilib qaytarsin lekin 1 yoki 2 harfdan iborat sozlarni esa oz holicha qoldirsin.
+MASALAN: capitalizeWords('name should be a string') return 'Name Should be a String'
  */
 
-
-function removeDuplicate(input: string): string {
-  const charMap = new Map<string, boolean>();
-  const result: string[] = [];
-
-  for (const char of input) {
-    if (!charMap.has(char)) {
-      charMap.set(char, true);
-      result.push(char);
-    }
-  }
-
-  return result.join('');
+function capitalizeWords(input: string): string {
+  return input
+    .split(" ")
+    .map((word) => {
+      if (word.length > 2) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+      return word;
+    })
+    .join(" ");
 }
 
-console.log(removeDuplicate("Helllllloworldddddd"));
-
+console.log(
+  capitalizeWords(
+    "everything around you that you call life was made up by people that were no smarter than you"
+  )
+);
