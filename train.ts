312acf66a06219788faf2a3962730c09cbfde4d5
tttemurbek <nonemenoneme9999@gmail.com>
2024-08-09 20:44:57 +0900

    feat: completed ZG-TASK

--- train.ts
+++ train.ts
@@ -1,24 +1,12 @@
 /**
-ZF-TASK:
+ ZG-TASK:
 
-Shunday function yozing, uni string parametri bolsin. String ichidagi har bir sozni bosh harflarini katta harf qilib qaytarsin lekin 1 yoki 2 harfdan iborat sozlarni esa oz holicha qoldirsin.
-MASALAN: capitalizeWords('name should be a string') return 'Name Should be a String'
+Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin. 
+MASALAN: capitalizeWords('name should be a string') return 'name_should_be_a_string'
  */
 
 function capitalizeWords(input: string): string {
-  return input
-    .split(" ")
-    .map((word) => {
-      if (word.length > 2) {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      }
-      return word;
-    })
-    .join(" ");
+  return input.toLowerCase().replace(/\s+/g, "_");
 }
 
-console.log(
-  capitalizeWords(
-    "everything around you that you call life was made up by people that were no smarter than you"
-  )
-);
+console.log(capitalizeWords("name should be a string"));
