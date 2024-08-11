2d729cfda4a5fea1a6e48f2ac43e4b93857857f4
tttemurbek <nonemenoneme9999@gmail.com>
2024-08-11 17:36:09 +0900

    feat: completed ZH-TASK

--- train.ts
+++ train.ts
@@ -1,12 +1,26 @@
 /**
- ZG-TASK:
+ ZH-TASK:
 
-Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin. 
-MASALAN: capitalizeWords('name should be a string') return 'name_should_be_a_string'
+Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin. 
+MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
  */
 
-function capitalizeWords(input: string): string {
-  return input.toLowerCase().replace(/\s+/g, "_");
+function findDisappearedNumbers(nums: number[]): number[] {
+  const maxValue = Math.max(nums.length, ...nums);
+  const present = new Array(maxValue);
+
+  for (let num of nums) {
+    present[num - 1] = true;
+  }
+
+  const missingNums = [];
+  for (let i = 0; i < maxValue; i++) {
+    if (!present[i]) {
+      missingNums.push(i + 1);
+    }
+  }
+
+  return missingNums;
 }
 
-console.log(capitalizeWords("name should be a string"));
+console.log(findDisappearedNumbers([1, 5, 7]));
